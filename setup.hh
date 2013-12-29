de9f99e6292bbe8a25c33f41c26bafe6b1b339c8
Mhoram Kerbin <mhoram.kerbin@gmx.de>
2013-12-30 00:14:30 +0100

    added docu for setup

--- setup.hh
+++ setup.hh
@@ -28,21 +28,31 @@
 #define NODES "[5, 30]"
 
 
-#define PLANET_MASS (5.2915793E22)
-#define PLANET_RADIUS (600000)
-#define PLANET_SCALE_HEIGHT (5000)
-#define PLANET_P_0 (1)
-#define PLANET_ROT_PER (21600)
-#define PLANET_SOI (84159286)
-#define PLANET_MAX_V (10000)
-#define LAUNCH_LATITUDE (-(0 + 5.0/60 + 49.0/3600)/180 * M_PI)
-#define LAUNCH_LONGITUDE 0 // +(-(74.0 + 33.0/60 + 27.0/3600)/180 * M_PI)
-#define LAUNCH_ELEVATION (77.1)
-#define TARGET_PERIAPSIS (75000)
+#define PLANET_MASS (5.2915793E22) // kg
+#define PLANET_RADIUS (600000) // m
+#define PLANET_SCALE_HEIGHT (5000) // m
+#define PLANET_P_0 (1) // atm
+#define PLANET_ROT_PER (21600) // s
+#define PLANET_SOI (84159286) // m
+#define PLANET_MAX_V (10000) // m/s
+#define LAUNCH_LATITUDE (-(0 + 5.0/60 + 49.0/3600)/180 * M_PI) // dimensionless
+#define LAUNCH_LONGITUDE 0 // +(-(74.0 + 33.0/60 + 27.0/3600)/180 * M_PI) // dimensionless
+#define LAUNCH_ELEVATION (77.1) // m
+#define TARGET_PERIAPSIS (75000) // m as altitude above sealevel
 
-#define STAGES (2)
+#define STAGES (2) // dimensionless number of stages
 
+// STAGE_PARAM contains an array of array of doubles
+// the first array contains information about the first stage
+//
+// The elements of the inner arrays containf the following information
+// 1. mass of fuel in kg
+// 2. total mass of ship at begin of the stage in kg
+// 3. thrust of the engines in kN
+// 4. ISP at 1atm in s
+// 5. ISP in vacuun in s
+// 6. drag coefficient (dimensionless)
+// 7. minimal duration of stage in s (will probably be removed)
 #define STAGE_PARAM {{4000, 9640, 200.0, 320, 370, 0.2, 58}, {2000, 3590,  50.0, 300, 390, 0.2, 117}}
 
-
-#define STAGE_TIME_MAX_FACTOR 10
+#define STAGE_TIME_MAX_FACTOR 10 // factor for maximum duration of stages
